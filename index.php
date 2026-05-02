d12205223e9008dca530c32942a5781240de6f67
Heart Anana <2023_cete_ananahea@online.htcgsc.edu.ph>
2026-05-02 22:37:42 +0800

    Fix homepage login redirect

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
 <?php
-echo password_hash("123456", PASSWORD_DEFAULT);
-?>
-
-<!--$_COOKIE
-Run: here, save_test_part.php and After this we will host this to cloud via render and TiBD, now go
-
--->
+header("Location: pages/login.php");
+exit();
